f1bf7b3340007ce68e7a387c6afb292aaac58d38
iqbalfachry19 <ifachry@rocketmail.com>
2023-05-01 01:46:46 +0700

    first

---
+++ components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex bg-gray-500 text-white bottom-0 absolute w-full space-x-2">
+    <h1>made by iqbal</h1>
+  </div>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <nav class="flex">
+      <ul class="flex space-x-2">
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
